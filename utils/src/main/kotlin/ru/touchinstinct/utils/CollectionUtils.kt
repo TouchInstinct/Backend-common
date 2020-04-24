e258f750b33d4d81873f2ecabf5ddc7063c94629
Mikhail Yasnov <yasnov.mi@gmail.com>
2020-04-24 19:33:40 +0300

    Delete unused function

--- utils/src/main/kotlin/ru/touchinstinct/utils/CollectionUtils.kt
+++ utils/src/main/kotlin/ru/touchinstinct/utils/CollectionUtils.kt
@@ -2,17 +2,5 @@ package ru.touchinstinct.utils
 
 import java.math.BigDecimal
 
-fun <T, F, R> Map<F, R>.remap(iterable: Iterable<T>, mappingKey: T.() -> F): Map<T, R> {
-    val result = HashMap<T, R>()
-    iterable.forEach {
-        val key = it.mappingKey()
-        val value = this[key]
-        if (value != null) {
-            result[it] = value
-        }
-    }
-    return result
-}
-
 inline fun <T> Iterable<T>.sumBy(selector: (T) -> BigDecimal) =
     fold(BigDecimal.ZERO) { accumulator, element -> accumulator + selector(element) }
