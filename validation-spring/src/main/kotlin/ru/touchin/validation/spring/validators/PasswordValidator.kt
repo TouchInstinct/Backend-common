4f61e105a794fd51b9b3d3ac146297854bcb7432
Mikhail Yasnov <yasnov.mi@gmail.com>
2021-06-21 20:33:31 +0300

    Move default values to constants

--- validation-spring/src/main/kotlin/ru/touchin/validation/spring/validators/PasswordValidator.kt
+++ validation-spring/src/main/kotlin/ru/touchin/validation/spring/validators/PasswordValidator.kt
@@ -6,13 +6,17 @@ import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class PasswordValidator(
-    @Value("\${validation.password.regex}")
+    @Value("\${validation.password.regex:$DEFAULT_PASSWORD_REGEX}")
     private val passwordValidationRegex: Regex
 ) : ConstraintValidator<Password, String> {
 
     override fun isValid(value: String?, context: ConstraintValidatorContext): Boolean {
         return value?.matches(passwordValidationRegex)
             ?: true
+    }
+
+    companion object {
+        private const val DEFAULT_PASSWORD_REGEX = "^\\S{6,20}$"
     }
 
 }
--- validation-spring/src/main/kotlin/ru/touchin/validation/spring/validators/PhoneNumberValidator.kt
+++ validation-spring/src/main/kotlin/ru/touchin/validation/spring/validators/PhoneNumberValidator.kt
@@ -6,13 +6,17 @@ import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class PhoneNumberValidator(
-    @Value("\${validation.phone.regex}")
+    @Value("\${validation.phone.regex:$DEFAULT_PHONE_REGEX}")
     private val phoneValidationRegex: Regex
 ) : ConstraintValidator<PhoneNumber, String> {
 
     override fun isValid(value: String?, context: ConstraintValidatorContext): Boolean {
         return value?.matches(phoneValidationRegex)
             ?: true
+    }
+
+    companion object {
+        private const val DEFAULT_PHONE_REGEX = "\\+7\\d{10}"
     }
 
 }
