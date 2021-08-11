7c73d2418cd357b1e5d74688754ffcd5a433bd65
Alexander Buntakov <alexander.buntakov@gmail.com>
2021-08-11 20:12:37 +0300

    add secure methods (#44)

--- common/src/main/kotlin/ru/touchin/common/byte/ByteUtils.kt
+++ common/src/main/kotlin/ru/touchin/common/byte/ByteUtils.kt
@@ -8,4 +8,12 @@ object ByteUtils {
         }
     }
 
+    fun decodeHex(hex: String): ByteArray {
+        check(hex.length % 2 == 0) { "Must have an even length" }
+
+        return hex.chunked(2)
+            .map { it.toInt(16).toByte() }
+            .toByteArray()
+    }
+
 }
