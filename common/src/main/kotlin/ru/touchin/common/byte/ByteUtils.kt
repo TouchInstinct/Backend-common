833bee0a60f631e5a42eca3639d2ce99d7432deb
Alexander Buntakov <alexander.buntakov@gmail.com>
2021-08-11 19:59:15 +0300

    fix PR issues

--- common/src/main/kotlin/ru/touchin/common/byte/ByteUtils.kt
+++ common/src/main/kotlin/ru/touchin/common/byte/ByteUtils.kt
@@ -8,10 +8,12 @@ object ByteUtils {
         }
     }
 
-    fun String.decodeHex(): ByteArray {
-        check(length % 2 == 0) { "Must have an even length" }
 
-        return chunked(2)
+
+    fun decodeHex(hex: String): ByteArray {
+        check(hex.length % 2 == 0) { "Must have an even length" }
+
+        return hex.chunked(2)
             .map { it.toInt(16).toByte() }
             .toByteArray()
     }
