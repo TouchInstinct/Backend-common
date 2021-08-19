faaf4f412144374db14cef11ebe9ae3b57a9ed59
Vasili Karaev <kratos661@gmail.com>
2021-08-19 18:49:47 +0300

    Add default spring web logger

---
+++ logger-spring-web/src/main/kotlin/ru/touchin/logger/spring/web/configurations/SpringLoggerWebConfiguration.kt
@@ -0,0 +1,6 @@
+package ru.touchin.logger.spring.web.configurations
+
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan("ru.touchin.logger.spring.web")
+class SpringLoggerWebConfiguration
