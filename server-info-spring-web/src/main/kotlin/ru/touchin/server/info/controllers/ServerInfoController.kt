e211caec2f976ddaeb8b86ee1e7fd2691e834ccd
Denis Kazantsev <denis.kazantsev@touchin.ru>
2022-04-08 17:39:53 +0300

    fixed controller

--- server-info-spring-web/src/main/kotlin/ru/touchin/server/info/controllers/ServerInfoController.kt
+++ server-info-spring-web/src/main/kotlin/ru/touchin/server/info/controllers/ServerInfoController.kt
@@ -2,14 +2,16 @@ package ru.touchin.server.info.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.touchin.server.info.services.ServerInfoService
 
-@RestController("/info")
+@RestController
+@RequestMapping("/info")
 class ServerInfoController {
 
     @Autowired
-    private lateinit var serverInfoServices: List<ServerInfoService>
+    private lateinit var serverInfoServices: List<ServerInfoService> //TODO(Move to constructor)
 
     @GetMapping
     fun getServerInfo(): List<Map<String, String>> {
