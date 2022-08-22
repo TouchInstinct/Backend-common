628c992b8d4575fca53d5fa8fb22bc469ca1c7ef
Artyom <17145209+Korna@users.noreply.github.com>
2022-08-22 15:28:51 +0300

    Add multiple options for FCM authorization (#79)
    * Add extra authorization types
    
    * Update readme file

---
+++ push-message-provider-fcm/src/main/kotlin/ru/touchin/push/message/provider/fcm/configurations/DateConverter.kt
@@ -0,0 +1,21 @@
+package ru.touchin.push.message.provider.fcm.configurations
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
+import org.springframework.core.convert.converter.Converter
+import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
+import java.util.*
+
+@ConfigurationPropertiesBinding
+@Component
+class DateConverter(
+    @Qualifier("push-message-provider.fcm.auth")
+    private val simpleDateFormat: SimpleDateFormat
+) : Converter<String, Date> {
+
+    override fun convert(source: String?): Date? {
+        return source?.let(simpleDateFormat::parse)
+    }
+
+}
--- push-message-provider-fcm/src/main/kotlin/ru/touchin/push/message/provider/fcm/configurations/PushMessageProviderFcmConfiguration.kt
+++ push-message-provider-fcm/src/main/kotlin/ru/touchin/push/message/provider/fcm/configurations/PushMessageProviderFcmConfiguration.kt
@@ -1,9 +1,12 @@
 package ru.touchin.push.message.provider.fcm.configurations
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.auth.oauth2.AccessToken
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
@@ -11,6 +14,8 @@ import org.springframework.context.annotation.Import
 import org.springframework.core.io.ClassPathResource
 import ru.touchin.push.message.provider.configurations.PushMessageProviderConfiguration
 import ru.touchin.push.message.provider.fcm.properties.PushMessageProviderFcmProperties
+import java.text.SimpleDateFormat
+import java.util.*
 
 @ComponentScan("ru.touchin.push.message.provider.fcm")
 @ConfigurationPropertiesScan(basePackages = ["ru.touchin.push.message.provider.fcm"])
@@ -19,9 +24,30 @@ class PushMessageProviderFcmConfiguration {
 
     @Bean
     fun firebaseMessaging(
-        properties: PushMessageProviderFcmProperties
+        properties: PushMessageProviderFcmProperties,
+        objectMapper: ObjectMapper
     ): FirebaseMessaging {
-        val credentials = GoogleCredentials.fromStream(ClassPathResource(properties.auth.resourcePath).inputStream)
+        val credentials = when {
+            properties.auth.credentialsData != null -> {
+                GoogleCredentials.fromStream(
+                    objectMapper.writeValueAsString(properties.auth.credentialsData).byteInputStream(Charsets.UTF_8)
+                )
+            }
+
+            properties.auth.credentialsFile != null -> {
+                GoogleCredentials.fromStream(
+                    ClassPathResource(properties.auth.credentialsFile.path).inputStream
+                )
+            }
+
+            properties.auth.token != null -> {
+                GoogleCredentials.create(
+                    AccessToken(properties.auth.token.value, properties.auth.token.expiresAt)
+                )
+            }
+
+            else -> throw IllegalStateException("No more authorization types allowed.")
+        }
 
         val options: FirebaseOptions = FirebaseOptions.builder()
             .setCredentials(credentials)
@@ -32,6 +58,12 @@ class PushMessageProviderFcmConfiguration {
         val firebaseApp: FirebaseApp = FirebaseApp.initializeApp(options, properties.appName)
 
         return FirebaseMessaging.getInstance(firebaseApp)
+    }
+
+    @Bean
+    @Qualifier("push-message-provider.fcm.auth")
+    fun simpleDateFormat(): SimpleDateFormat {
+        return SimpleDateFormat("yyyy-MM-dd HH:mm:ss X", Locale.getDefault())
     }
 
 }
