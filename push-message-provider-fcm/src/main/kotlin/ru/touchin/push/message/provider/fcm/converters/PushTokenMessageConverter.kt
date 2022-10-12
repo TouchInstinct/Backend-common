aaad0290aab50005d92487acd6dcf9f34ddc6e9c
Korna <17145209+Korna@users.noreply.github.com>
2022-10-12 13:42:16 +0300

    Add notification sound for iOS and Android

--- push-message-provider-fcm/src/main/kotlin/ru/touchin/push/message/provider/fcm/converters/PushTokenMessageConverter.kt
+++ push-message-provider-fcm/src/main/kotlin/ru/touchin/push/message/provider/fcm/converters/PushTokenMessageConverter.kt
@@ -1,5 +1,9 @@
 package ru.touchin.push.message.provider.fcm.converters
 
+import com.google.firebase.messaging.AndroidConfig
+import com.google.firebase.messaging.AndroidNotification
+import com.google.firebase.messaging.ApnsConfig
+import com.google.firebase.messaging.Aps
 import com.google.firebase.messaging.Message
 import org.springframework.stereotype.Component
 import ru.touchin.push.message.provider.dto.Notification
@@ -10,9 +14,17 @@ class PushTokenMessageConverter(
     private val notificationConverter: NotificationConverter
 ) {
 
+    private companion object {
+
+        const val PLATFORMS_KEY_SOUND_ON = "default"
+
+    }
+
     operator fun invoke(request: PushTokenMessage): Message {
         return Message.builder()
             .setToken(request.token)
+            .setupApns()
+            .setupAndroid()
             .setIfExists(request.notification)
             .putAllData(request.data)
             .build()
@@ -24,6 +36,30 @@ class PushTokenMessageConverter(
         } else {
             this
         }
+    }
+
+    private fun Message.Builder.setupApns(): Message.Builder {
+        return setApnsConfig(
+            ApnsConfig.builder()
+                .setAps(
+                    Aps.builder()
+                        .setSound(PLATFORMS_KEY_SOUND_ON)
+                        .build()
+                )
+                .build()
+        )
+    }
+
+    private fun Message.Builder.setupAndroid(): Message.Builder {
+        return setAndroidConfig(
+            AndroidConfig.builder()
+                .setNotification(
+                    AndroidNotification.builder()
+                        .setSound(PLATFORMS_KEY_SOUND_ON)
+                        .build()
+                )
+                .build()
+        )
     }
 
 }
