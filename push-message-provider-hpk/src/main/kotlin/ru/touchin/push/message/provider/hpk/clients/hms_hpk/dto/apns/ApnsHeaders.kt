145639baab38f4daefcbf8b1a457e06287e86506
Korna <17145209+Korna@users.noreply.github.com>
2022-11-07 17:20:48 +0300

    Add APNS related DTO's

---
+++ push-message-provider-hpk/src/main/kotlin/ru/touchin/push/message/provider/hpk/clients/hms_hpk/dto/apns/ApnsHeaders.kt
@@ -0,0 +1,108 @@
+package ru.touchin.push.message.provider.hpk.clients.hms_hpk.dto.apns
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import ru.touchin.push.message.provider.hpk.base.builders.Buildable
+import ru.touchin.push.message.provider.hpk.clients.hms_hpk.enums.apns.ApnsPriority
+
+@JsonNaming(PropertyNamingStrategies.KebabCaseStrategy::class)
+internal data class ApnsHeaders private constructor(
+    val authorization: String?,
+    val apnsId: String?,
+    val apnsExpiration: Long?,
+    @JsonProperty("apns-priority")
+    val apnsPriority: ApnsPriority?,
+    val apnsTopic: String?,
+    val apnsCollapseId: String?,
+) {
+
+    class Validator {
+
+        fun check(apnsHeaders: ApnsHeaders) {
+            with(apnsHeaders) {
+                if (authorization != null) {
+                    require(
+                        authorization.matches(AUTHORIZATION_PATTERN)
+                    ) { "authorization must start with bearer" }
+                }
+                if (apnsId != null) {
+                    require(apnsId.matches(APN_ID_PATTERN)) { "apns-id format error" }
+                }
+                if (apnsCollapseId != null) {
+                    require(
+                        apnsCollapseId.toByteArray().size < 64
+                    ) { "Number of apnsCollapseId bytes should be less than 64" }
+                }
+            }
+        }
+
+        private companion object {
+
+            private val AUTHORIZATION_PATTERN: Regex = Regex("^bearer*")
+            private val APN_ID_PATTERN: Regex = Regex("[0-9a-z]{8}(-[0-9a-z]{4}){3}-[0-9a-z]{12}")
+
+        }
+
+    }
+
+    class Builder : Buildable {
+
+        private var authorization: String? = null
+        private var apnsId: String? = null
+        private var apnsExpiration: Long? = null
+        private var apnsPriority: ApnsPriority? = null
+        private var apnsTopic: String? = null
+        private var apnsCollapseId: String? = null
+
+        fun setAuthorization(authorization: String): Builder {
+            this.authorization = authorization
+            return this
+        }
+
+        fun setApnsId(apnsId: String): Builder {
+            this.apnsId = apnsId
+            return this
+        }
+
+        fun setApnsExpiration(apnsExpiration: Long): Builder {
+            this.apnsExpiration = apnsExpiration
+            return this
+        }
+
+        fun setApnsPriority(apnsPriority: ApnsPriority): Builder {
+            this.apnsPriority = apnsPriority
+            return this
+        }
+
+        fun setApnsTopic(apnsTopic: String): Builder {
+            this.apnsTopic = apnsTopic
+            return this
+        }
+
+        fun setApnsCollapseId(apnsCollapseId: String): Builder {
+            this.apnsCollapseId = apnsCollapseId
+            return this
+        }
+
+        fun build(): ApnsHeaders {
+            return ApnsHeaders(
+                authorization = authorization,
+                apnsId = apnsId,
+                apnsExpiration = apnsExpiration,
+                apnsPriority = apnsPriority,
+                apnsTopic = apnsTopic,
+                apnsCollapseId = apnsCollapseId,
+            )
+        }
+    }
+
+    companion object {
+
+        val validator = Validator()
+
+        fun builder() = Builder()
+
+    }
+
+}
---
+++ push-message-provider-hpk/src/main/kotlin/ru/touchin/push/message/provider/hpk/clients/hms_hpk/dto/apns/ApnsHmsOptions.kt
@@ -0,0 +1,37 @@
+package ru.touchin.push.message.provider.hpk.clients.hms_hpk.dto.apns
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import ru.touchin.push.message.provider.hpk.base.builders.Buildable
+import ru.touchin.push.message.provider.hpk.clients.hms_hpk.enums.android.AndroidTargetUserType
+
+internal data class ApnsHmsOptions private constructor(
+    @JsonProperty("target_user_type")
+    val androidTargetUserType: AndroidTargetUserType,
+) {
+
+    class Validator {
+
+        fun check(apnsHmsOptions: ApnsHmsOptions) {
+            // no validation
+        }
+
+    }
+
+    class Builder : Buildable {
+
+        fun build(androidTargetUserType: AndroidTargetUserType): ApnsHmsOptions {
+            return ApnsHmsOptions(
+                androidTargetUserType = androidTargetUserType,
+            )
+        }
+    }
+
+    companion object {
+
+        val validator = Validator()
+
+        fun builder() = Builder()
+
+    }
+
+}
