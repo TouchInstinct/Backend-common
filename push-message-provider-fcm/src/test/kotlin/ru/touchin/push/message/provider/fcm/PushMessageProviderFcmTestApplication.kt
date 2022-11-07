74104a7413cd9e2f382c6ea0ba41353cbf64d97c
Korna <17145209+Korna@users.noreply.github.com>
2022-11-07 16:08:27 +0300

    Fix test invokations

--- push-message-provider-fcm/src/test/kotlin/ru/touchin/push/message/provider/fcm/PushMessageProviderFcmTestApplication.kt
+++ push-message-provider-fcm/src/test/kotlin/ru/touchin/push/message/provider/fcm/PushMessageProviderFcmTestApplication.kt
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.google.firebase.FirebaseApp
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
+import org.springframework.context.event.ContextRefreshedEvent
 import java.text.SimpleDateFormat
 
 @TestConfiguration
 @SpringBootConfiguration
 @EnablePushMessageProviderFcm
-class PushMessageProviderFcmTestApplication {
+class PushMessageProviderFcmTestApplication : ApplicationListener<ContextRefreshedEvent> {
 
     @Bean
     fun objectMapper(
@@ -26,6 +29,14 @@ class PushMessageProviderFcmTestApplication {
             setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
             dateFormat = simpleDateFormat
         }
+    }
+
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        clearSingletonsOutsideContainer()
+    }
+
+    private fun clearSingletonsOutsideContainer() {
+        FirebaseApp.getApps().forEach(FirebaseApp::delete)
     }
 
 }
