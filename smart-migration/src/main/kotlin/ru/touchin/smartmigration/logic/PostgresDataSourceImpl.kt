8b9643b60adb91478777a80c4b3f7d5b444a3fd9
Ilia Ravin <ilai.rav664@gmail.com>
2022-11-10 17:20:05 +0300

    indent fix

--- smart-migration/src/main/kotlin/ru/touchin/smartmigration/logic/PostgresDataSourceImpl.kt
+++ smart-migration/src/main/kotlin/ru/touchin/smartmigration/logic/PostgresDataSourceImpl.kt
@@ -8,11 +8,13 @@ class PostgresDataSourceImpl: DataSourceSQL {
 
     @Language("SQL")
     override fun getTableCheckSQL(): String {
-        return """CREATE TABLE IF NOT EXISTS smart_migration  ( 
-            ID BIGSERIAL PRIMARY KEY , 
-            BUILD_NUMBER VARCHAR(255) NOT NULL, 
-            DATE timestamp NOT NULL
-            );"""
+        return """
+                  CREATE TABLE IF NOT EXISTS smart_migration  (
+                  ID BIGSERIAL PRIMARY KEY, 
+                  BUILD_NUMBER VARCHAR(255) NOT NULL,
+                  DATE timestamp NOT NULL
+                  );
+               """
     }
 
     @Language("SQL")
@@ -22,8 +24,10 @@ class PostgresDataSourceImpl: DataSourceSQL {
 
     @Language("SQL")
     override fun getInsertMigrationSQL(buildNumber: String, formattedTime: String): String {
-        return """INSERT INTO $MIGRATION_TABLE_NAME (BUILD_NUMBER, DATE)
-            VALUES ('$buildNumber', '$formattedTime');"""
+        return """
+                  INSERT INTO $MIGRATION_TABLE_NAME (BUILD_NUMBER, DATE)
+                  VALUES ('$buildNumber', '$formattedTime');
+               """
     }
 
 }
--- smart-migration/src/main/kotlin/ru/touchin/smartmigration/logic/SqlDataSourceImpl.kt
+++ smart-migration/src/main/kotlin/ru/touchin/smartmigration/logic/SqlDataSourceImpl.kt
@@ -8,14 +8,16 @@ class SqlDatasourceImpl:DataSourceSQL {
 
     @Language("TSQL")
     override fun getTableCheckSQL(): String {
-        return """IF NOT EXISTS (
-            SELECT * FROM sysobjects WHERE name = '$MIGRATION_TABLE_NAME' and xtype='U'
-            )
-            CREATE TABLE SMART_MIGRATION  (
-            ID BIGINT PRIMARY KEY IDENTITY , 
-            BUILD_NUMBER VARCHAR(255) NOT NULL,
-            DATE DATETIME NOT NULL
-            );"""
+        return """
+                  IF NOT EXISTS (
+                      SELECT * FROM sysobjects WHERE name = '$MIGRATION_TABLE_NAME' and xtype='U'
+                  )
+                  CREATE TABLE SMART_MIGRATION  (
+                  ID BIGINT PRIMARY KEY IDENTITY , 
+                  BUILD_NUMBER VARCHAR(255) NOT NULL,
+                  DATE DATETIME NOT NULL
+                  );
+               """
     }
 
     @Language("TSQL")
@@ -25,8 +27,10 @@ class SqlDatasourceImpl:DataSourceSQL {
 
     @Language("TSQL")
     override fun getInsertMigrationSQL(buildNumber: String, formattedTime: String): String {
-        return """INSERT INTO $MIGRATION_TABLE_NAME (BUILD_NUMBER, DATE)
-            VALUES ('$buildNumber', '$formattedTime');"""
+        return """
+                  INSERT INTO $MIGRATION_TABLE_NAME (BUILD_NUMBER, DATE)
+                  VALUES ('$buildNumber', '$formattedTime');
+               """
     }
 
 }
