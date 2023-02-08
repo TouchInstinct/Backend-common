0769a2afad054a6ae315689dbb5e1f0064c34825
Alexander Buntakov <alexander.buntakov@gmail.com>
2023-02-09 00:47:56 +0300

    add slow test annotation

--- common-spring-test/src/main/kotlin/ru/touchin/common/spring/test/annotations/SlowTest.kt
+++ common-spring-test/src/main/kotlin/ru/touchin/common/spring/test/annotations/SlowTest.kt
@@ -2,7 +2,15 @@
 package ru.touchin.common.spring.test.annotations
 
 import org.springframework.context.annotation.Profile
+import org.springframework.test.context.junit.jupiter.DisabledIf
 
 @Profile(value = ["test", "test-slow"])
 @Target(allowedTargets = [AnnotationTarget.FUNCTION, AnnotationTarget.CLASS])
+@DisabledIf(
+    expression = """
+       #{systemProperties['tests.slow.enabled'] != null
+        ? systemProperties['tests.slow.enabled'].toLowerCase().contains('false')
+        : false} 
+    """
+)
 annotation class SlowTest
